26a988d74790f3d0724ef373bb1103b8c3e920ca
Antonio Pires <apsistemas@oi.com.br>
2018-09-01 19:14:33 -0300

    metodos listar

--- index.php
+++ index.php
@@ -8,8 +8,30 @@ require_once("config.php");
 
 //echo json_encode($usuarios);
 
-$root = new Usuario();
 
-$root->loadById(1);
+// chama um usuario
+//$root = new Usuario();
 
-echo $root;
+//$root->loadById(1);
+
+//echo $root;
+
+
+//chama uma lista de usuarios
+
+//$lista = Usuario::getList();
+
+//echo json_encode($lista);
+
+
+//chama uma lista de usuarios buscando pelo login
+
+//$search = Usuario::search("jo");
+
+//echo json_encode($search);
+
+//chama um usuario usando login e senha
+$usuario = new Usuario();
+$usuario->login("joao", "12345");
+
+echo $usuario;
