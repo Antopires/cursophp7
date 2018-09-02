a94bd708b88ec2684a2dfa88ea9b4411aa521fde
Antonio Pires <apsistemas@oi.com.br>
2018-09-02 19:42:30 -0300

    inserir e alterar

--- index.php
+++ index.php
@@ -31,7 +31,28 @@ require_once("config.php");
 //echo json_encode($search);
 
 //chama um usuario usando login e senha
+//$usuario = new Usuario();
+//$usuario->login("joao", "12345");
+
+//echo $usuario;
+
+
+//Inserindo um novo usuario no banco de dados
+
+//$aluno = new Usuario("aluno2", "@lun1");  //opção 2
+
+//$aluno->setDeslogin("aluno"); //opção 1
+//$aluno->setDessenha("@lun0"); //opção 1
+//$aluno->insert();
+//echo $aluno;
+
+
+//Fazendo um updadte no banco de dados
+
 $usuario = new Usuario();
-$usuario->login("joao", "12345");
+
+$usuario->loadById(6);
+
+$usuario->update("professor", "pro123");
 
 echo $usuario;
